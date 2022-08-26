3cc07135e4d2b82775f390d903a6a3785ba34d04
dolsen0 <derek.olsen@gmail.com>
2022-08-26 11:13:54 -0400

    added firestore db to api

--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,11 +1,31 @@
-export function getTasks(req, res){
+import dbConnect from "./dbConnect.js";
+
+
+export async function getTasks(req, res){ //add userID and timestamp later
+    const db = dbConnect();
+    db.collection('tasks').get()
+    const collection = await db.collection('tasks').get()
+        .catch(err => res.status(500).send(err));
+    const tasks = collection.docs.map(doc => {
+        let task = doc.data()
+        task.id = doc.id
+        return task;
+    })
     res.send('tasks')
 }
 
-export function createTask(req,res){
+export async function createTask(req,res){ //add userID and timestamp later
     const newTask = req.body;
-    res.status(201).send('Task Added');
-}
+    if (!newTask || !newTask.task) {
+        res.status(400).send({success: false, message: 'Invalid request'});
+        return;
+    }
+    const db = dbConnect();
+    await db.collection('tasks').add(newTask)
+      .catch(err => res.status(500).send(err));
+    res.status(201);
+    getTasks(req, res); // send back the full list of tasks...
+  }
 
 export function updateTask(req,res){
     const taskUpdate = req.body;
